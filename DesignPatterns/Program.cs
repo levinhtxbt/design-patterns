8c581b2b806acf4f0874820db2110757c6f2a0c4
Vinh Le <levinhtxbt@gmail.com>
2018-08-19 12:58:01 +0700

    init

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.SOLID;
+
+namespace DesignPatterns
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var openClosedPrinciple = new OpenClosedPrinciple.AppyPrinciple();
+            openClosedPrinciple.Execute();
+
+
+            Console.ReadKey();
+
+            
+        }
+
+
+    }
+
+    
+}
+
---
+++ DesignPatterns/SOLID/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SOLID
+{
+    public class SingleResponsibilityPrinciple
+    {
+        public class Journal
+        {
+            private readonly List<string> _entries = new List<string>();
+            private static int _count = 0;
+
+            public int AddNew(string entry)
+            {
+                _entries.Add($"{++_count}: " + entry);
+                return _count;
+            }
+
+            public void RemoveEntry(int id)
+            {
+                _entries.RemoveAt(id);
+            }
+
+            public override string ToString()
+            {
+                return string.Join(Environment.NewLine, _entries);
+            }
+
+        }
+
+        public class Persistence
+        {
+            public void Save(Journal journal, string fileName)
+            {
+                File.WriteAllText(fileName, journal.ToString());
+            }
+
+            public Journal Load(string file)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Journal Load(Uri uri)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+
+}
